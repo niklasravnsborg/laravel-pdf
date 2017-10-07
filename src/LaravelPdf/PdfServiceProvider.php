0b3cff0afa226a8206041b1bb2dbc878f14e09ff
Erfan Sahafnejad <erfan.sahaf@gmail.com>
2017-10-08 01:26:56 +0330

    feat: add auto-publishing ability
    Now, users can publish `pdf.php` config file by running `php artisan vendor:publish` command.

--- src/LaravelPdf/PdfServiceProvider.php
+++ src/LaravelPdf/PdfServiceProvider.php
@@ -14,6 +14,19 @@ class PdfServiceProvider extends BaseServiceProvider {
 	 */
 	protected $defer = false;
 
+
+	/*
+	* Bootstrap the application service
+	*
+	* @return void
+	*/
+	public function boot()
+	{
+		$this->publishes([
+            	__DIR__ . '/../config/pdf.php' => config_path('pdf.php'),
+        	]);
+    	}
+	
 	/**
 	 * Register the service provider.
 	 *
