9fa2b33aaeba35e0029ae3dfb9ba40b447b55321
omid <omidevil200@gmail.com>
2021-02-08 07:43:31 +0330

    add defaultCssFile to cinfig

--- src/config/pdf.php
+++ src/config/pdf.php
@@ -8,5 +8,7 @@ return [
 	'keywords'              => '',
 	'creator'               => 'Laravel Pdf',
 	'display_mode'          => 'fullpage',
-	'tempDir'               => base_path('../temp/')
+	'tempDir'               => base_path('../temp/'),
+	'defaultCssFile' => base_path('public/css/pdf.css'),
+
 ];
